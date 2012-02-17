e21f60e499b1501cce4e95ea42f9ae139040e7b4
Stuart Liston <stuart.liston@gmail.com>
2012-02-17 11:35:51 +1100

    Stripping out geokit

--- lib/app/models/geo_regioning/base.rb
+++ lib/app/models/geo_regioning/base.rb
@@ -1,13 +1,4 @@
 class GeoRegioning::Base < ActiveRecord::Base
   self.abstract_class = true
-  include Geokit
   
-  acts_as_mappable :distance_field_name => :distance,
-                   :lat_column_name     => :geo_latitude,
-                   :lng_column_name     => :geo_longitude,
-                   :auto_geocode        => true
-
-  def lat_long
-    [self.geo_latitude, self.geo_longitude]
-  end
 end
--- rails/init.rb
+++ rails/init.rb
@@ -12,8 +12,6 @@ if defined?(ActionView)
 end
 
 require 'active_support'
-require 'geokit'
-require 'geokit-rails'
 require 'geo_regioning'
 
 #Load the config
@@ -22,19 +20,3 @@ if File.exists?(File.join(RAILS_ROOT, 'config', 'geo_regioning.yml'))
 else
   GeoRegioning::config = YAML.load_file(File.join(RAILS_ROOT, 'vendor', 'plugins','geo_regioning', 'lib', 'config', 'geo_regioning.yml'))
 end
-
-#set the Geokit options
-Geokit::Geocoders::yahoo                  = GeoRegioning.config['geokit_options']['yahoo'] rescue false
-Geokit::Geocoders::google                 = GeoRegioning.config['geokit_options']['google'] rescue false
-Geokit::Geocoders::geocoder_us            = GeoRegioning.config['geokit_options']['geocoder_us'] rescue false
-Geokit::Geocoders::geocoder_ca            = GeoRegioning.config['geokit_options']['geocoder_ca'] rescue false
-Geokit::Geocoders::provider_order         = GeoRegioning.config['geokit_options']['provider_order'] rescue []
-## there is timeout and request timeout depending on the gem version:
-if Geokit::Geocoders.respond_to?(:request_timeout)
-  Geokit::Geocoders::request_timeout        = GeoRegioning.config['geokit_options']['request_timeout'] rescue 3
-else
-  Geokit::Geocoders::timeout        = GeoRegioning.config['geokit_options']['request_timeout'] rescue 3
-end
-
-Geokit::default_units                     = GeoRegioning.config['geokit_options']['units'].to_sym rescue :miles
-Geokit::default_formula                   = GeoRegioning.config['geokit_options']['formula'].to_sym rescue :sphere
