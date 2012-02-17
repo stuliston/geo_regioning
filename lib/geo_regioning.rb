b4e80f257d46900c7983116a35035ac1c0283f4a
Stuart Liston <stuart.liston@gmail.com>
2012-02-17 11:55:53 +1100

    removed more deprecations

--- lib/geo_regioning.rb
+++ lib/geo_regioning.rb
@@ -3,7 +3,7 @@
   path = File.join(File.dirname(__FILE__), 'app', dir)
   $LOAD_PATH << path
   ActiveSupport::Dependencies.autoload_paths << path
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
+  ActiveSupport::Dependencies.autoload_once_paths.delete(path)
 end
 
 #Make the models happy
